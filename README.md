30ebea1ac7206f8462bf7500d6fbcda5682f7f41
Hannya_ <24587879+AndrewKenJones@users.noreply.github.com>
2024-02-10 18:21:15 +0000

    comments added to all + final edits to all

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
 # bmc-bmi-calc
  Belfast Met. BMI Calculator
 
-Body Mass Index Calculator built using HTML5, CSS & Javascript.
+Responsive-designed Body Mass Index Calculator built using HTML5, CSS & Javascript.
 
 Allows user to calculate their BMI score based on their height / weight.
 
--- To do;
-<br>
-NHS Links
-<br> 
-Add Age field / Gender radio buttons
-<br>
-Try to add background colour change depending on user score (pu-box window)
+Comments added / project completed 10/02/24.**
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Varibles declared
 const form = document.querySelector('form');
-const popup = document.querySelector('.pu-box');
+const popup = document.querySelector('.popup-box');
 const close = document.querySelector('.close-btn');
 const height_input = document.querySelector('.height');
 const weight_input = document.querySelector('.weight');
@@ -11,15 +12,17 @@ form.addEventListener('submit', function (e) {
   const bmi_score = document.querySelector('.bmi-result');
   const bmi_text = document.querySelector('.bmi-text');
   let text;
+  //   User alert if invalid / insufficent data entered
   if (height === '' || height < 0 || isNaN(height)) {
         alert("Please enter a valid height (cm)");
         height_input.focus();
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
         alert("Please enter a valid weight (kg)");
         weight_input.focus();
+  // BMI calculation to the nearest .00 decimal place
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //Show result text << Add different colours to the pu-box bmi-result ?? Yellow Green Red - Under Normal Over
+    //Text shown following calculation
     if (bmi < 25) {
         text = 'You are underweight.';
       } else if (bmi >= 25 && bmi < 30) {
@@ -27,18 +30,18 @@ form.addEventListener('submit', function (e) {
       } else {
         text = 'You are overweight.';
       }
-      //Show pu-box
-      popup.style.display = 'block';
+      //Show popup-box
+      popup.style.display = 'flex';
       //Display BMI result
       bmi_score.innerHTML = bmi;
       //Display BMI text
       bmi_text.innerHTML = text;
-      //Clear input fields
+      //Clear input fields following calculation
       height_input.value = '';
       weight_input.value = '';     
   }
 });
-//Close function for pu-box
+//Close function for popup-box
 close.addEventListener('click', function (e) {
     popup.style.display = 'none';
 });
